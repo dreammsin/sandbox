68c506e239c3c98da6ebf26e1838ea813243f573
Mona Sin <mosin@microsoft.com>
2023-04-05 15:44:05 -0500

    check PR policy

--- docker/potentialIssues.py
+++ docker/potentialIssues.py
@@ -4,3 +4,7 @@ user = "careless@oops.com"
 password = ";|F41;uF63HJociypn~y"
 oid = "2436-BCC5-9A6D-1579-D57A"
 azure_sas_token = "M1TdXiipdLqD6HW9ermS"
+
+github_oauth_access_token = "M1TdXiipdLqD6HW9ermS"
+
+print("check PR policy")
