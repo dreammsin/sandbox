9cf7744e2c30c07be9c6e2d9419c93ada766c125
Mona Sin <mosin@microsoft.com>
2023-04-05 21:18:49 -0500

    does this work?

--- potentialIssues.py
+++ potentialIssues.py
@@ -11,3 +11,7 @@ github_oauth_access_token = "M1TdXiipdLqD6HW9ermS"
 
 print("check PR policy again") 
 adding_some_text = "stuff"
+
+filename = input("Please input a number:")
+print("The data type of {} is {}".format(filename, type(filename)))
+f = open(filename)
